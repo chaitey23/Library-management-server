d32616e6a2eb651ab04216f22a77ebac4ff6def4
chaitey50 <chaiteyc@gmail.com>
2025-09-07 22:05:42 +0600

    update .env file

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const admin = require("firebase-admin");
 
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g,'\n')
 
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
